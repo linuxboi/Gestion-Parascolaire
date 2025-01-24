bcd5ce4531bc1444b0eebf60f65b82848ba39200
ALAOUI <zakariaziko3040@gmail.com>
2025-01-24 20:25:00 +0100

    last7 commit

--- backend/models/etudiant.js
+++ backend/models/etudiant.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
 const etudiantSchema = new mongoose.Schema({
   nom: { type: String, required: true },
